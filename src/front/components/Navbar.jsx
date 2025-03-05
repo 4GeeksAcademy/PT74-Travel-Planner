96f6f0988a2df57622888608915db1baca27a59b
Gabriel Zavarse <zavarsegabriel@gmail.com>
2025-03-05 22:38:02 +0000

    travel planer

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom";
 import travelImg from "../assets/img/travel.png";
+import "./Navbar.css";
 
 export const Navbar = () => {
-
 	return (
-		<nav className="navbar navbar-light bg-primary nav-justified">
-			<button class="btn btn-primary-subtle fa-solid fa-bars nav-item" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"> Menu</button>
-			<div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
-				<div class="offcanvas-header">
-					<h5 class="offcanvas-title" id="offcanvasScrollingLabel">Travel Like A Rockstar!!</h5>
-					<button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+		<nav className="navbar custom-navbar">
+			{/* Botón del menú lateral */}
+			<button className="btn btn-menu" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">
+				☰ Menu
+			</button>
+
+			{/* Menú lateral (Offcanvas) */}
+			<div className="offcanvas offcanvas-start" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+				<div className="offcanvas-header">
+					<h5 className="offcanvas-title">Travel Like A Rockstar!!</h5>
+					<button type="button" className="btn-close" data-bs-dismiss="offcanvas"></button>
 				</div>
-				<ul class="offcanvas-body">
+				<ul className="offcanvas-body list-unstyled">
 					<li className="my-1">Add Travel Companions</li>
 					<li className="my-1">Favorite Destinations</li>
 					<li className="my-1">View Packing List</li>
@@ -19,17 +24,19 @@ export const Navbar = () => {
 					<li className="my-1">View Itinerary</li>
 				</ul>
 			</div>
-			<div className="container nav-item d-flex justify-content-center">
-				<Link to="/">
-					<img
-						src={travelImg} alt="Travel Logo"
-						className="logo-img"/>				
-					<span className="navbar-brand mb-0 h1">Travel Planner</span>
+
+			{/* Logo y nombre */}
+			<div className="navbar-logo">
+				<Link to="/" className="text-white text-decoration-none">
+					<img src={travelImg} alt="Travel Logo" className="logo-img"/>
+					<span className="navbar-brand">Travel Planner</span>
 				</Link>
 			</div>
-			<div className="ml-auto nav-item">
+
+			{/* Botón de inicio de sesión */}
+			<div>
 				<Link to="/login">
-					<button className="btn btn-primary">Sign In / Register</button>
+					<button className="btn btn-auth">Sign In / Register</button>
 				</Link>
 			</div>
 		</nav>
--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,75 +1,107 @@
-import React, { useEffect } from "react"
-import './style.css';
+import React from "react";
+import "./style.css";
 
 export const Home = () => {
-	return (
-	<div class="bg-dark position-relative">	
-	<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner mb-4">
-      <div class="carousel-item active">
-        <img src="https://cdn.prod.website-files.com/5d9667bad4a41e222995e15b/62a28855d2adb643754bcb64_Grand%20Cayman.jpg" class="d-block w-100" alt="..."/>
-      </div>
-      <div class="carousel-item">
-        <img src="https://www.travelandleisure.com/thmb/Qa7_o8_XVpIVH5vqq7i73UlTSkU=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/eiffel-tower-paris-france-EIFFEL0217-6ccc3553e98946f18c893018d5b42bde.jpg" class="d-block w-100" alt="..."/>
-      </div>
-      <div class="carousel-item">
-        <img src="https://themilepost.com/wp-content/uploads/2022/05/Peyto_Lake-Banff_NP-Canada.jpg" class="d-block w-100" alt="..."/>
-      </div>
-    </div>
-  </div>
-  <div class="card mb-4 bg-primary-subtle">
-  <div class="row">
-    <div class="col-4">
-      <img src="https://plus.unsplash.com/premium_photo-1683121257579-d40449389b63?q=80&w=3269&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" alt="..."/>
-    </div>
-    <div class="col my-auto">
-      <div class="card-body text-center">
-        Destinations
-      </div>
-    </div>
-  </div>
-</div>
-<div class="card mb-4 bg-primary-subtle">
-  <div class="row">
-    <div class="col-4">
-      <img src="https://plus.unsplash.com/premium_photo-1706189731991-39c4e4697d05?q=80&w=3272&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" alt="..."/>
-    </div>
-    <div class="col my-auto">
-      <div class="card-body text-center">
-        Itinerary
-      </div>
-    </div>
-  </div>
-</div>
-<div class="card mb-4 bg-primary-subtle">
-  <div class="row">
-    <div class="col-4">
-      <img src="https://images.unsplash.com/photo-1553729459-efe14ef6055d?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" alt="..."/>
-    </div>
-    <div class="col my-auto">
-      <div class="card-body text-center">
-        Expenses
-      </div>
-    </div>
-  </div>
-</div>
-<div class="card mb-4 bg-primary-subtle">
-  <div class="row">
-    <div class="col-4">
-      <img src="https://images.unsplash.com/photo-1484480974693-6ca0a78fb36b?q=80&w=3272&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" alt="..."/>
-    </div>
-    <div class="col my-auto">
-      <div class="card-body text-center">
-        Packing List
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-	);
+  return (
+    <div className="bg-dark">
+     
 
+      {/* Carousel */}
+      <div
+        id="carouselExampleSlidesOnly"
+        className="carousel slide slideshow-background"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              src="https://cdn.prod.website-files.com/5d9667bad4a41e222995e15b/62a28855d2adb643754bcb64_Grand%20Cayman.jpg"
+              className="d-block w-100"
+              alt="..."
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://www.travelandleisure.com/thmb/Qa7_o8_XVpIVH5vqq7i73UlTSkU=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/eiffel-tower-paris-france-EIFFEL0217-6ccc3553e98946f18c893018d5b42bde.jpg"
+              className="d-block w-100"
+              alt="..."
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://themilepost.com/wp-content/uploads/2022/05/Peyto_Lake-Banff_NP-Canada.jpg"
+              className="d-block w-100"
+              alt="..."
+            />
+          </div>
+        </div>
+      </div>
 
-}
+      {/* Secciones de contenido */}
+      <div className="content-wrapper">
+        <div className="card content">
+          <div className="row">
+            <div className="col-4">
+              <img
+                src="https://plus.unsplash.com/premium_photo-1683121257579-d40449389b63?q=80&w=3269&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div className="col my-auto">
+              <div className="card-body text-center">Destinations</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="card content">
+          <div className="row">
+            <div className="col-4">
+              <img
+                src="https://plus.unsplash.com/premium_photo-1706189731991-39c4e4697d05?q=80&w=3272&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div className="col my-auto">
+              <div className="card-body text-center">Itinerary</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="card content">
+          <div className="row">
+            <div className="col-4">
+              <img
+                src="https://images.unsplash.com/photo-1553729459-efe14ef6055d?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div className="col my-auto">
+              <div className="card-body text-center">Expenses</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="card content">
+          <div className="row">
+            <div className="col-4">
+              <img
+                src="https://images.unsplash.com/photo-1484480974693-6ca0a78fb36b?q=80&w=3272&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div className="col my-auto">
+              <div className="card-body text-center">Packing List</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 
 
